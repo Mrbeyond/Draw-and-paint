9c4f0bd56ad965a3406ee6ff8d4ca9ebef8c025f
Beyond <abiolaolatunji007@gmail.com>
2019-04-30 15:32:56 +0100

    Stroke and Fill functions improvised

--- draw.js
+++ draw.js
@@ -1,6 +1,8 @@
 rec=false; crc=false; up=false; ryt=false;lef=false; don=false; lin=false;
+fill=false; stroke=false;
 var cav=document.getElementById('mycanvas');
 var draw=cav.getContext('2d');
+var draw2=cav.getContext('2d');
 
 function loader(){if (screen.width<450) {cav.width=screen.width} }
 
@@ -10,7 +12,7 @@ function loader(){if (screen.width<450) {cav.width=screen.width} }
    p.innerHTML= " Mousedown point is: "+x + " ," + y ;
   console.log( x + " ," + y ); }
  
-function upp(event) {
+function upp(event){
    w = event.offsetX;
    h = event.offsetY;
     p.innerHTML= "Mousedown point is: "+x+", "+y+" &nbsp, Mouseup point is: "+w +", " +h ;
@@ -19,69 +21,83 @@ function upp(event) {
 if (ryt){ cright()};
 if (don){ cdown()};
 if (crc){ circ()};
- if (rec){ rect()};
+ if (rec){
+  if (fill) {rect(); draw2.fillRect(x,y,w,h); draw2.closePath(); }
+  if (stroke){rect(); draw.strokeRect(x,y,w,h); draw.closePath()}   };
 if (lin){ line()};
 if (up){ cup()};
-if (lef){ cleft()}; }
+if (lef){ cleft()}; styling();}
+
+
+function styling(){
+	if (stroke){
+		if (rec){ draw.stroke()};
+	  console.log(stroke); return draw.stroke(); } 
+
+	if (fill){
+	 if (rec){draw2.fill()};
+	 console.log(fill); return draw.fill(); } }
 
 function line(){
 draw.strokeStyle = color.value;
+draw.fillStyle = color.value;
 draw.beginPath();
 draw.moveTo(x,y);
-draw.lineTo(w,h);
-draw.stroke(); }
+draw.lineTo(w,h);} 
 
 function circ(){
 draw.strokeStyle = color.value;
+draw.fillStyle = color.value;
 d= Math.sqrt( ((w-x)**2)+((h-y)**2))
 draw.beginPath();
 draw.arc(((x+w)/2),((y+h)/2), (d/2),0, 2*Math.PI,false);
-draw.stroke(); }
+ }
 
  function cup(){
  	draw.strokeStyle = color.value;
+ 	draw.fillStyle = color.value;
  	c1=y+100; c2=h+100;
  	if (y>346) { c1=c1-50; c2=c2-50;}
  	draw.beginPath();
  	draw.moveTo(x,y);
- 	draw.bezierCurveTo(x,c1,w,c2,w,h) ;
- 	draw.stroke();   }
+ 	draw.bezierCurveTo(x,c1,w,c2,w,h);
+ 	   }
 
  function cdown(){
  	draw.strokeStyle = color.value;
+ 	draw.fillStyle = color.value;
  	c1=y-100; c2=h-100;
  	if (y<100) { c1=c1+50; c2=c2+50;}
  	draw.beginPath();
  	draw.moveTo(x,y);
- 	draw.bezierCurveTo(x,c1,w,c2,w,h) ;
- 	draw.stroke();   }
+ 	draw.bezierCurveTo(x,c1,w,c2,w,h); 	 }
 
  function cright(){
  	draw.strokeStyle = color.value;
+ 	draw.fillStyle = color.value;
  	c1=x-100; c2=w-100;
  	if (x<100){ c1=c1+50; c2=c2+50;}
  	draw.beginPath();
  	draw.moveTo(x,y);
- 	draw.bezierCurveTo(c1,y,c2,h,w,h) ;
-	draw.stroke();  }
+ 	draw.bezierCurveTo(c1,y,c2,h,w,h); 	 }
 
 function cleft(){
  	draw.strokeStyle = color.value;
+ 	draw.fillStyle = color.value;
  	c1=x+100; c2=w+100;
  	if (x>524){ c1=c1-50; c2=c2-50.}
  	draw.beginPath();
  	draw.moveTo(x,y);
- 	draw.bezierCurveTo(c1,y,c2,h,w,h) ;
-	draw.stroke();	}
+ 	draw.bezierCurveTo(c1,y,c2,h,w,h) ;	}
 
 function rect(){
 draw.strokeStyle = color.value;
+draw2.fillStyle = color.value;
 if (w>=x){  w= w-x} else{if(w<x){ w=-(x-w)}};
-if (h>=y){  h= h-y} else{if(h<y){h=-(y-h)}};
-draw.strokeRect(x,y,w,h); } 
+if (h>=y){  h= h-y} else{if(h<y){h=-(y-h)}};  } 
 
 function see(event) {
 aa= event.offsetX;
 bb= event.offsetY;
- demo.innerHTML = aa + " , " + bb;
-}
+ demo.innerHTML = aa + " , " + bb; } 
+
